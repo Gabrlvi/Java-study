488fad86e101ce41b856e2e1f1f2f6e6e762b22d
Gabrlvi <ghbviana@gmail.com>
2024-10-17 08:46:20 -0300

    Add files via upload

---
+++ EstudoModulo.java
@@ -0,0 +1,17 @@
+public class Main {
+    
+    public static void main (String[]args){
+        
+        System.out.println("antes de chamar o metodo");
+        imprimirInformações();
+        System.out.println("depois de chamar o metodo");
+    }
+    
+    public static void imprimirInformações(){
+        
+        System.out.println("==================================");
+        System.out.println("Logica de programação");
+        System.out.println("Aprendo a programar");
+        System.out.println("==================================");
+    }
+}
